f1cedb64b34045c8c2897902f02ab1e98add463c
Niki <nikolett.papp92@gmail.com>
2021-04-20 21:21:09 +0200

    files has been modified and added.

--- js/js.js
+++ js/js.js
@@ -1,17 +1,8 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+$(document).ready(function () {
+    $('.mobilmenu').click(function () {
+        $('.navbar').toggleClass("menunyit");
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+    });
+});
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
+
